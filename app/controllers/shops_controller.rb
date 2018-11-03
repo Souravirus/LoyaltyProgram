71fcaf0ca10e8cae0551c7601ba8bc88d34472bb
souravirus <souravsahoo66@yahoo.com>
2018-11-03 16:52:53 +0530

    Added the controller for calculating free food items

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,9 +3,17 @@ class ShopsController < ApplicationController
   end
   def query
     @ans = ""
-    @shop_name = params['name']  
-    @user_name = params['user_name']
-    @product_name = params['product_name']
+    loyalty = 1
+    shop_name = params['name']  
+    user_name = params['user_name']
+    product_name = params['product_name']
+
+    product_amt = Shop.product_count(shop_name, user_name, product_name)
+    loyalty = Shop.loyalty_count(product_name, shop_name)
+
+    if loyalty!= 0
+        @ans = product_amt/loyalty
+    end
   end
   def search
   end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,17 @@
 class Shop < ApplicationRecord
+    def self.product_count(shop_name, user_name, product_name)
+        orders = Order.where(user_name: user_name, 
+                             product_name: product_name,
+                             name: shop_name
+                            )
+        orders.sum(:product_amt)
+        
+    end
+
+    def self.loyalty_count(product_name, shop_name)
+        loyalty_amt = Product.select(:product_amt)
+                             .where(product_name: product_name,
+                                    name: shop_name)
+        loyalty_amt.sum(:product_amt)
+    end
 end
